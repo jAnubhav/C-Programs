5e709768ffce7a1f09bf976cd41f933aab227e34
Anubhav Jain <87580533+jAnubhav@users.noreply.github.com>
2023-12-30 22:02:00 +0530

    Add files via upload

---
+++ Arrays/array-operations.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+
+int arr[100], size, i;
+
+void insertElement()
+{
+    int pos, elem, flag = 0;
+    printf("\nInserting Element\n");
+    while (1)
+    {
+        printf("\nEnter Position : ");
+        scanf("%d", &pos);
+        if (pos < size)
+            break;
+        else
+            printf("\nPosition cannot be greater than size.\n");
+    }
+
+    printf("\nEnter Element : ");
+    scanf("%d", &elem);
+
+    for (i = size++; i > pos; i--)
+        arr[i] = arr[i - 1];
+    arr[pos] = elem;
+
+    printf("\nElement Inserted Successfully.\n");
+}
+
+void deleteElement()
+{
+    int elem, flag = 0;
+    printf("\nDeleting Element\n\nEnter Element : ");
+    scanf("%d", &elem);
+
+    for (i = 0; i < size; i++)
+    {
+        if (arr[i] == elem)
+        {
+            flag = 1;
+            size--;
+        }
+        arr[i] = arr[i + flag];
+    }
+
+    printf("\n%s.\n", (flag) ? "Element Deleted Successfully" : "Element Not Found");
+}
+
+void displayAll()
+{
+    printf("\nThe elements of the array are : ");
+    for (i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+}
+
+int main()
+{
+    int ch, flag = 1;
+    printf("Enter the size : ");
+    scanf("%d", &size);
+
+    for (i = 0; i < size; i++)
+    {
+        printf("Enter element %d : ", i + 1);
+        scanf("%d", (arr + i));
+    }
+
+    while (flag)
+    {
+        printf("\nMenu-driven program:\n1) Insert Element\n2) Delete Element\n3) Display Elements\n4) Exit");
+        printf("\n\nEnter choice : ");
+        scanf("%d", &ch);
+
+        switch (ch)
+        {
+        case 1:
+            insertElement();
+            break;
+        case 2:
+            deleteElement();
+            break;
+        case 3:
+            displayAll();
+            break;
+        case 4:
+            printf("\nExiting...");
+            flag = 0;
+            break;
+        default:
+            printf("\nEnter a Valid Option\n");
+        }
+    }
+    return 0;
+}
